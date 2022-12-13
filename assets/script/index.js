ee2e4c19feaff58469de42cdc7299b9f170eaf12
Enjae Antonio <Enjaeantonio@outlook.com>
2022-12-12 18:03:59 -0600

    finished layout

--- assets/script/index.js
+++ assets/script/index.js
@@ -30,3 +30,46 @@ function sleep(duration){
       setTimeout(resolve, duration)
   });
 };
+
+const loadScreen = select('.load-screen');
+const myMap = select('#map')
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZW5qYWUiLCJhIjoiY2xiZ3Jvd2h4MDl5YjN3bW1nNDVmbTc3aiJ9.7a11htvZ_4iZBrcr47Aubw';
+
+function getLocation(position){
+        const { longitude, latitude } = position.coords;
+
+        const map = new mapboxgl.Map({
+                container: 'map',
+                center: [longitude, latitude],
+                style: 'mapbox://styles/mapbox/streets-v12',
+                zoom: 17
+                });
+                
+                const marker = new mapboxgl.Marker({
+                        color: "#89CFF5 ",
+                        draggable: true
+                        }).setLngLat([longitude, latitude])
+                        .addTo(map);
+};
+
+function errorHandler(error) {
+        console.log(error.message);
+};
+
+const options = {
+        enableHighAccuracy: true,
+        showUserLocation: true
+};
+
+
+if (navigator.geolocation) {
+        const geo = navigator.geolocation;
+        geo.getCurrentPosition(getLocation, errorHandler, options);
+        loadScreen.classList.remove('hidden')
+        sleep(5000).then(() => myMap.classList.remove('hidden'))
+
+}else {
+        console.log('Geolocation is not supported by your old browser');
+}
